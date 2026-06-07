edf26112f671e4fc02fe25fbd64d72705738f6b0
S <192511283.simats@saveetha.com>
2026-06-07 20:16:27 +0530

    Implement state validation in Missionaries and Cannibals
    Added validation function to check missionaries and cannibals state.

--- Missionaries and Cannibals.py
+++ Missionaries and Cannibals.py
@@ -1,32 +1,43 @@
 from collections import deque
 
-start = (3,3,1)
-goal = (0,0,0)
+def is_valid(m, c):
+    if m < 0 or c < 0 or m > 3 or c > 3:
+        return False
 
-moves = [(1,0),(2,0),(0,1),(0,2),(1,1)]
+    if m > 0 and m < c:
+        return False
 
-q = deque([(start, [])])
-visited = set()
+    if (3 - m) > 0 and (3 - m) < (3 - c):
+        return False
 
-while q:
-    state, path = q.popleft()
+    return True
 
-    if state == goal:
-        print(path + [state])
-        break
+def bfs():
+    start = (3, 3, 1)
+    goal = (0, 0, 0)
 
-    if state in visited:
-        continue
-    visited.add(state)
+    queue = deque([(start, [start])])
+    visited = {start}
 
-    m,c,b = state
+    moves = [(1,0), (2,0), (0,1), (0,2), (1,1)]
 
-    for dm,dc in moves:
-        if b:
-            nm,nc,nb = m-dm,c-dc,0
-        else:
-            nm,nc,nb = m+dm,c+dc,1
+    while queue:
+        (m, c, b), path = queue.popleft()
 
-        if 0<=nm<=3 and 0<=nc<=3:
-            if (nm==0 or nm>=nc) and (3-nm==0 or 3-nm>=3-nc):
-                q.append(((nm,nc,nb), path+[state]))
+        if (m, c, b) == goal:
+            return path
+
+        for dm, dc in moves:
+            if b == 1:
+                nm, nc, nb = m-dm, c-dc, 0
+            else:
+                nm, nc, nb = m+dm, c+dc, 1
+
+            if is_valid(nm, nc) and (nm, nc, nb) not in visited:
+                visited.add((nm, nc, nb))
+                queue.append(((nm, nc, nb), path + [(nm, nc, nb)]))
+
+solution = bfs()
+
+for state in solution:
+    print(state)
